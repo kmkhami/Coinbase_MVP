4c7943fad46903eee17349dfff37f32e733bb7ea
Nick Khami <nicholas.khami@gmail.com>
2021-08-24 13:13:49 -0500

    react router update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AccountConnect from './components/AccountConnectButton.js'; 
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
       </header>
+      <Router>
+        <Switch>
+          <Route path = '/:code'> 
+          </Route>  
+          <Route path = '/'>
+          <AccountConnect />
+          </Route> 
+        </Switch> 
+      </Router>
     </div>
   );
 }
