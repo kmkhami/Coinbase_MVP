9dab42e002a70514d18d2dfcd95e2b05ab235260
Ryan Stachelczyk <ryan.stachelczyk2@nelnet.net>
2021-08-25 11:19:44 -0400

    added react-redux state management

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import logo from './assets/paymentspring_logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from './store/index';
 
 function App() {
+
+  const state = useSelector((state) => state.coinbase);
+  const dispatch = useDispatch();
+
+  const { updateJSON } = bindActionCreators(actionCreators, dispatch);
+  console.log(state)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <h1>{JSON.stringify(state)}</h1>
+      <button onClick={() => updateJSON( { id: 1, code: 123, public_key: 567 })}> UpdateJSON </button>
     </div>
   );
 }
