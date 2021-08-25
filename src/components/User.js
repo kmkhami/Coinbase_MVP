1b95351cd35a0afcdbc3942a750a7e94f4021d5c
Ryan Stachelczyk <ryan.stachelczyk2@nelnet.net>
2021-08-25 18:31:24 -0400

    Update redux state attemp 1

--- src/components/User.js
+++ src/components/User.js
@@ -2,18 +2,26 @@ import '../stylesheets/user.css';
 import QueryString from "query-string";
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from '../store/index';
 
 function User(props) {
     const params = QueryString.parse(props.location.search);
-    const [isLoading, setLoading] = useState(true); 
+    const [isLoading, setLoading] = useState(true);
     const [accessToken, setAccessToken] = useState(null);
 
+    const json = useSelector((state) => state.coinbase);
+    const dispatch = useDispatch();
+
     useEffect(() => {
+        const { updateJSON } = bindActionCreators(actionCreators, dispatch);
+
         const requestParams = {
             code: params.code,
             grant_type: 'authorization_code',
-            client_id: process.env.REACT_APP_CLIENT_ID, 
-            client_secret: process.env.REACT_APP_CLIENT_SECRET, 
+            client_id: process.env.REACT_APP_CLIENT_ID,
+            client_secret: process.env.REACT_APP_CLIENT_SECRET,
             redirect_uri: process.env.REACT_APP_REDIRECT_URI
         }
 
@@ -22,13 +30,14 @@ function User(props) {
         )
         .then(response => {
             setAccessToken(response.data);
+            updateJSON(response);
             setLoading(false);
         })
         .catch(function (error) {
             setAccessToken("");
             setLoading(false);
-        }); 
-    }, [isLoading, params.code]);
+        });
+    }, [isLoading, params.code, dispatch]);
 
     if(isLoading) {
         return <div className='App'>
@@ -39,6 +48,7 @@ function User(props) {
     return (
         <div className="User">
             {accessToken.access_token}
+            {console.log(json)}
         </div>
     );
 }
