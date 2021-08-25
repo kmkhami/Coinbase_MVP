117c1395d1f9ef072467922055dd7ba9713691bb
Nick Khami <nicholas.khami@gmail.com>
2021-08-25 16:34:21 -0500

    Working version of login button

--- src/components/User.js
+++ src/components/User.js
@@ -1,13 +1,44 @@
 import '../stylesheets/user.css';
-import QueryString from "query-string"
+import QueryString from "query-string";
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 function User(props) {
     const params = QueryString.parse(props.location.search);
+    const [isLoading, setLoading] = useState(true); 
+    const [accessToken, setAccessToken] = useState(null);
 
-    
+    useEffect(() => {
+        const requestParams = {
+            code: params.code,
+            grant_type: 'authorization_code',
+            client_id: process.env.REACT_APP_CLIENT_ID, 
+            client_secret: process.env.REACT_APP_CLIENT_SECRET, 
+            redirect_uri: process.env.REACT_APP_REDIRECT_URI
+        }
+
+        axios.post('https://api.coinbase.com/oauth/token',
+            requestParams
+        )
+        .then(response => {
+            setAccessToken(response.data);
+            setLoading(false);
+        })
+        .catch(function (error) {
+            setAccessToken("");
+            setLoading(false);
+        }); 
+    }, [isLoading, params.code]);
+
+    if(isLoading) {
+        return <div className='App'>
+          <p> Loading </p>
+        </div>
+    }
+
     return (
         <div className="User">
-            {console.log(params.code)}
+            {accessToken.access_token}
         </div>
     );
 }
