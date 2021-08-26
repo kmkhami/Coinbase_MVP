56c9d8265b2107aaa74854a383ea9f2f9b695acf
Ryan Stachelczyk <ryan.stachelczyk2@nelnet.net>
2021-08-26 12:10:31 -0400

    Finished functionality on second screen

--- src/components/Pledge.js
+++ src/components/Pledge.js
@@ -1,11 +1,14 @@
 import '../stylesheets/pledge.css';
 import Select from 'react-select';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { FcCurrencyExchange } from "react-icons/fc";
+import axios from 'axios';
 
 function Pledge() {
   const [crypto, setCrypto] = useState('');
   const [amount, setAmount] = useState(0.00);
   const [convertedCrypto, setConvertedCrypto] = useState('');
+  const [currentRate, setCurrentRate] = useState(0);
 
   const crypto_coins = [
     { value: 'BTC', label: 'Bitcoin (BTC)'},
@@ -19,25 +22,35 @@ function Pledge() {
     { value: 'BCH', label: 'Bitcoin Cash (BCH)'},
     { value: 'LINK', label: 'Chainlink (LINK)'},
   ]
-
-  const handleSelection = selectedOption => {
-    setCrypto(selectedOption.value);
+  const updateCrypto = selectedCrypto => {
+    setCrypto(selectedCrypto.value);
+    axios.get(`https://api.coinbase.com/v2/exchange-rates?currency=${selectedCrypto.value}`)
+      .then(res => {
+        const body = res.data;
+        setCurrentRate(parseFloat(body.data.rates.USD));
+      })
+      .catch( e => {
+        console.log(e);
+      });
   }
 
-  const updateAmount = (amount) => {
-    setAmount(amount);
-    setConvertedCrypto(`~ $${amount * 48923}`);
-  }
+  useEffect(() => {
+    setConvertedCrypto(`~$${amount * currentRate}`)
+  }, [currentRate, amount])
 
   return (
     <div className='Pledge'>
       <p className='text-select'>Select Your Crypto</p>
-      <Select className='dropdown' options={crypto_coins} onChange={handleSelection}></Select>
+      <Select className='dropdown' options={crypto_coins} onChange={updateCrypto}></Select>
       <p className='text-donate'>Enter Donation Amount</p>
-      <input className='input' type='number' step='0.001' value={amount} onChange={(e) => updateAmount(e.target.value)}></input>
-      {crypto}
-      <br></br>
-      {convertedCrypto}
+      <div className='exchange'>
+        <input className='input' type='number' step='0.001' value={amount} onChange={(e) => setAmount(e.target.value)}></input>
+        {crypto}
+        <FcCurrencyExchange className="exchange-icon"></FcCurrencyExchange>
+        <br></br>
+        {convertedCrypto}
+        {'USD'}
+      </div>
     </div>
   );
 }
